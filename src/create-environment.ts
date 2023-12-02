b54be0b11c17a7b80e03c9790327c2b56976281b
Robert Ledang <rled@equinor.com>
2023-12-02 18:24:27 +0100

    Test changes

--- src/create-environment.ts
+++ src/create-environment.ts
@@ -47,12 +47,19 @@ async function getComponentConfig(components: Component[], env: string, branch?:
         const config = { ...template };
         config.environment = env;
 
+        // if (!state.options.useRadixCI) {
+        //     if (config.imageTagName) {
+        //         config.imageTagName = config.imageTagName.replace('{ENVIRONMENT}', env);
+        //     } else if (!branch) {
+        //         config.imageTagName = env;
+        //     }
+        // }
+
         if (!state.options.useRadixCI) {
-            if (config.imageTagName) {
-                config.imageTagName = config.imageTagName.replace('{ENVIRONMENT}', env);
-            } else if (!branch) {
-                config.imageTagName = env;
-            }
+            config.imageTagName = 'hmmm';
+        }
+        else {
+            config.imageTagName = 'oioiu';
         }
 
         const variables = await getVariables(comp.name, env);
